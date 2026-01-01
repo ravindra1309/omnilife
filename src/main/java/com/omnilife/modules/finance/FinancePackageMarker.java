35e8583a74d58d50bdae052697a0fa6271bd2278
ravindra1309 <ravindrasinghravi9@gmail.com>
2026-01-01 17:24:41 +0530

    Initial commit: Setup Spring Modulith scaffolding

---
+++ src/main/java/com/omnilife/modules/finance/FinancePackageMarker.java
@@ -0,0 +1,10 @@
+package com.omnilife.modules.finance;
+
+/**
+ * Package marker class for the finance module.
+ * This class ensures the finance package is created and can be used for module identification.
+ */
+public class FinancePackageMarker {
+    // Package marker - no implementation needed
+}
+
